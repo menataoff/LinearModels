1777d189f51f574261c778cd1618ff6e1ba502cd
Alexey <myk1n.alex@gmail.com>
2026-02-19 15:18:22 +0300

    Resctructured: mv base_model.py to core

--- linear_models/__init__.py
+++ linear_models/__init__.py
@@ -1,9 +1,3 @@
 """
 LinearModels - implementation of module linear_models.
 """
-
-from .base_model import LinearModel
-
-__all__ = ['LinearModel']
-
-__version__ = '0.1.0'
---
+++ linear_models/core/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module core - base classes for linear models
+"""
+
+from .base_model import LinearModel
+
+__all__ = ['LinearModel']
+
+__version__ = '0.1.0'
