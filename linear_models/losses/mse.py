d088b1b6a4924cf2915701a2e92e793fa52e19e8
Alexey <myk1n.alex@gmail.com>
2026-02-19 16:14:38 +0300

    Added: losses/mae

--- linear_models/losses/mse.py
+++ linear_models/losses/mse.py
@@ -11,8 +11,10 @@ class MSE(BaseLoss):
     def gradient(self, X: np.ndarray, y: np.ndarray, y_pred: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         n = X.shape[0]
 
-        gradient_weights = (2 / n) * X.T@(y_pred - y)
-        gradient_bias = (2 / n) * np.sum(y_pred - y)
+        error = y_pred - y
+
+        gradient_weights = (2 / n) * X.T@error
+        gradient_bias = (2 / n) * np.sum(error)
 
         return gradient_weights, gradient_bias
 
