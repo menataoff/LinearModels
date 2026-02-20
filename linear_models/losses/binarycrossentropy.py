732910a66bcc4a0b426510da8aca50ec3bba729d
Alexey <myk1n.alex@gmail.com>
2026-02-20 21:30:18 +0300

    Ready BCE

--- linear_models/losses/binarycrossentropy.py
+++ linear_models/losses/binarycrossentropy.py
@@ -6,18 +6,17 @@ import numpy as np
 
 class BinaryCrossEntropy(BaseLoss):
     def __call__(self, y: np.ndarray, y_pred: np.ndarray) -> float:
-        return -np.sum(y*np.log(y_pred) + (1-y)*np.log(1-y_pred))
-
-    def _sigmoid(self, y_pred: np.ndarray) -> float:
-        return np.float32(1 / (1 + np.exp(-y_pred)))
+        eps = 1e-15
+        y_pred = np.clip(y_pred, eps, 1 - eps) #0 and 1 - critical values. Сlipping to avoid log(0
+        return -np.mean(y*np.log(y_pred) + (1-y)*np.log(1-y_pred))
 
     def gradient(self, X: np.ndarray, y: np.ndarray, y_pred: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         n = X.shape[0]
 
         error = y_pred - y
 
-        gradient_weights = (2 / n) * X.T@error
-        gradient_bias = (2 / n) * np.sum(error)
+        gradient_weights = (1 / n) * X.T@error
+        gradient_bias = (1 / n) * np.sum(error)
 
         return gradient_weights, gradient_bias
 
